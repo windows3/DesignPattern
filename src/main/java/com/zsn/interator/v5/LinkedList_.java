5d92f5f3275f4eeee946c7602932dd79e5c5110f
windows3 <416400304@qq.com>
2020-05-20 00:51:36 +0800

    iterator v5 增加linkedListIterator

--- src/main/java/com/zsn/interator/v5/LinkedList_.java
+++ src/main/java/com/zsn/interator/v5/LinkedList_.java
@@ -1,11 +1,17 @@
 package com.zsn.interator.v5;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 /**
  * 相比数组，这个容器不用考虑边界问题，可以动态扩展
  */
 class LinkedList_ implements Collection_ {
     Node head = null;
     Node tail = null;
+    LinkedList<Object> list = new LinkedList<Object>();
+
     //目前容器中有多少个元素
     private int size = 0;
 
@@ -13,14 +19,17 @@ class LinkedList_ implements Collection_ {
         Node n = new Node(o);
         n.next = null;
 
-        if(head == null) {
+        if (head == null) {
             head = n;
             tail = n;
         }
 
         tail.next = n;
         tail = n;
+//        map.put(String.valueOf(size), n.getO());
+        list.add(n);
         size++;
+
     }
 
     private class Node {
@@ -30,6 +39,10 @@ class LinkedList_ implements Collection_ {
         public Node(Object o) {
             this.o = o;
         }
+
+        public Object getO() {
+            return o;
+        }
     }
 
     public int size() {
@@ -38,6 +51,26 @@ class LinkedList_ implements Collection_ {
 
     @Override
     public Iterator_ iterator() {
-        return null;
+
+        return new LinkedListIterator();
+    }
+
+    private class LinkedListIterator implements Iterator_ {
+
+        private int currentIndex = 0;
+
+        @Override
+        public boolean hasNext() {
+            if (currentIndex >= size) return false;
+            return true;
+        }
+
+        @Override
+        public Object next() {
+            Node n = (Node) list.get(currentIndex);
+            Object o = n.getO();
+            currentIndex++;
+            return o;
+        }
     }
 }
--- src/main/java/com/zsn/interator/v5/Main.java
+++ src/main/java/com/zsn/interator/v5/Main.java
@@ -13,7 +13,20 @@ import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
-        Collection_ list = new ArrayList_();
+//        Collection_ list = new ArrayList_();
+//        for(int i=0; i<15; i++) {
+//            list.add(new String("s" + i));
+//        }
+//        System.out.println(list.size());
+//
+//        //这个接口的调用方式：
+//        Iterator_ it = list.iterator();
+//        while(it.hasNext()) {
+//            Object o = it.next();
+//            System.out.println(o);
+//        }
+
+         Collection_ list = new LinkedList_();
         for(int i=0; i<15; i++) {
             list.add(new String("s" + i));
         }
